7776cd120add73387ff7b26082d56b610faf3be7
MacBook Ninja <MacbookNinja@users.noreply.github.com>
2023-04-11 14:50:37 +0800

    Add files via upload

--- prod/js/grid_withSorting_4col.js
+++ prod/js/grid_withSorting_4col.js
@@ -891,11 +891,6 @@ let NintexSamplegridRepeatingSection = _decorate([e$2('grid-activitylog-4col')],
               title: 'Allow Sorting',
               description: 'Select true to allow sorting'
             },
-            pagination: {
-              type: 'boolean',
-              title: 'Allow Pagination',
-              description: 'Select true to allow pagination'
-            },
 			Data: {
               type: 'string',
               title: 'Data to be rendered in the grid',
@@ -940,8 +935,7 @@ let NintexSamplegridRepeatingSection = _decorate([e$2('grid-activitylog-4col')],
             name: this.Col4
           }],
           data: JSON.parse(this.Data),
-          pagination: this.pagination {limit:20},
-		  search: true,
+		  pagination: true,
           sort: this.sortable
         });
         this.grid.render(this.shadowRoot?.getElementById('js-canvas'));
@@ -966,8 +960,7 @@ let NintexSamplegridRepeatingSection = _decorate([e$2('grid-activitylog-4col')],
               name: this.Col4
             }],
             sort: this.sortable,
-            pagination: this.pagination {limit:20},
-            search: true,
+			pagination: true,
             data: JSON.parse(this.Data)
           });
           this.grid.forceRender();
